f1fc7e16bb0ad16e91fd389a7d0f23bbdeb05390
epson121 <traxdater12@gmail.com>
2012-11-17 15:54:53 +0100

    installed new versions of gems and added rspec

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121117000417) do
+ActiveRecord::Schema.define(:version => 20121117115402) do
+
+  create_table "comments", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "comment"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "name"
